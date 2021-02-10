5fe5f2e98060d0a69f1e9a438980a3f26e3adb0c
yellowHuang <hlady1115@gmail.com>
2021-02-10 15:04:44 +0800

    get data from five days ago

--- futures.py
+++ futures.py
@@ -1,8 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
-r = requests.get(
-    'https://www.taifex.com.tw/cht/3/futContractsDate?queryType=1&goDay=&doQuery=1&dateaddcnt=&queryDate=2021%2F02%2F05&commodityId=')
-if r.status_code == requests.codes.ok:
-    soup = BeautifulSoup(r.text, 'html.parser')
-    print(soup.prettify())
+
+def crawl(date):
+    r = requests.get(
+        'https://www.taifex.com.tw/cht/3/futContractsDate?queryType=1&goDay=&doQuery=1&dateaddcnt=&queryDate={}%2F{}%2F{}'.format(date.year, date.month, date.day))
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        print('successfully got data from ', date)
+    else:
+        print('connect error')
+
+
+date = datetime.today()
+# get data from five days ago
+while True:
+    crawl(date)
+    date = date - timedelta(days=1)
+    if date < datetime.today() - timedelta(days=5):
+        break
